1d2fddcf8b8139030e990d7f03f4d17d524eb050
CSH111 <seongho6022@naver.com>
2023-11-11 23:39:09 +0900

    test

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template('./index.html')
+
+
+@app.route("/api/getFile")
+def xx():
+    return "zxzxzxzx"
