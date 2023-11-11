fbe6481183a457fcec9038ef1f747a89a3a54251
CSH111 <seongho6022@naver.com>
2023-11-12 00:11:55 +0900

    test55555555

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    print("index come!")
     return render_template('./index.html')
 
 
-@app.route("/api/getFile")
-def xx():
-    return "zxzxzxzx"
+# @app.route("/api/getFile")
+# def xx():
+#     return "zxzxzxzx"
 
 
-@app.route("/my")
-def xx():
-    return "mymymymy"
+# @app.route("/my")
+# def xx():
+#     return "mymymymy"
